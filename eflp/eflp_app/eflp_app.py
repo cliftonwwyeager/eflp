7e66a91093e530f9acc216ce02a15596f35188b8
Clifton W. W. Yeager <55325353+cliftonwwyeager@users.noreply.github.com>
2025-04-07 15:41:24 -0500

    v0.0.9-1
    Changed debug mode default option to false.

--- eflp/eflp_app/eflp_app.py
+++ eflp/eflp_app/eflp_app.py
@@ -434,4 +434,4 @@ def export_csv():
                     headers={"Content-Disposition": f"attachment;filename={filename}"})
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=False)
