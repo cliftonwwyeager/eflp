664414d13a8820f3c8e2ba8fbe27af8198f72dbc
Clifton W. W. Yeager <55325353+cliftonwwyeager@users.noreply.github.com>
2026-03-09 00:06:49 -0500

    v0.1.3-1

--- eflp/eflp_app/eflp_app.py
+++ eflp/eflp_app/eflp_app.py
@@ -1185,7 +1185,7 @@ def index():
     </div>
     """
     content = case_box + upload_form
-    return render_page("EFLP", "EFLP v0.1.1", content)
+    return render_page("EFLP", "EFLP v0.1.3", content)
 
 @app.route("/upload", methods=["POST"])
 def upload():
@@ -1573,3 +1573,4 @@ def export_csv():
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=False)
+
